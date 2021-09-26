1b72ae135eaa31ba072bcb5dc124ab8c9c2a32ff
yutaka.miyazaki <mono.12dev@gmail.com>
2021-09-26 18:20:05 +0900

    chore: add config

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 7,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+			modules: true,
+		},
+		project: ['./tsconfig.json'],
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'prettier',
+	],
+	plugins: ['prettier', 'react'],
+	rules: {
+		'react/display-name': 'off',
+		'react/prop-types': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+		'no-inner-declarations': 'off',
+	},
+	settings: {
+		react: {
+			version: 'detect', // detect the version of React to use
+		},
+	},
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Switch>
+			<Route path="/">
+				<div>/ path</div>
+			</Route>
+			<Route path="/user">
+				<div>/user path</div>
+			</Route>
+			<Route path="/user/article">
+				<div>/ user article</div>
+			</Route>
+		</Switch>
+	)
 }
 
-export default App;
+export default App
